81107091b2fb0d512d3940e663ac13d06131c0fc
lydiafernandes <fdes.lydia@gmail.com>
2018-07-14 14:26:47 +0530

    jpeg contact added

---
+++ contact.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<HTML>
+    <head>
+        <title> Contact Us </title>
+
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- popper.js is recommended for bootstrap -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+<!-- this file is included in the externa file -->
+<link rel="stylesheet" type="text/css" href="style.css"> 
+
+
+</head>
+<body> 
+    <div class="container contact-form">
+            <h1> Contact Form</h1>
+            <hr>
+            <div class="row">
+
+          
+
+            <div class="col-md-6">
+                <address> 5, Mahapalika Marg, Mumbai, 400001 </address>
+                <p>Email: fhtwbconference@xaviers.edu</p>
+                <p>Contact: 022 2262 0661</p>
+            </div> <!-- end of col-md-6 -->
+
+            <div class="col-md-6">
+                    <div class="form-group">
+                        <label> Name</label>
+                        <input type="text" class="form-control" name="" id="">
+                    </div>
+
+                    <div class="form-group">
+                        <label> Email</label>
+                        <input type="text" class="form-control" name="" id="">
+                    </div>
+
+                     <div class="form-group">
+                        <label>Message</label>
+                        <textarea rows="5" class="form-control" name="" id=""></textarea>
+                    </div>
+
+                     <div class="form-group">
+                        <button class="btn btn-primary btn-block">Send</button>
+                    </div>
+            </div> <!-- end of col-md-6 -->
+
+            </div> <!-- end of row class-->
+
+        </div> <!-- end of container class--> 
+</body>
+</HTML>
