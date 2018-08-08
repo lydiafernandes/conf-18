e9418ef782b21157b34877212cd866114c5656be
lydiafernandes <fdes.lydia@gmail.com>
2018-08-08 12:41:37 +0530

    about.php

--- includes/footer.php
+++ includes/footer.php
@@ -21,29 +21,6 @@
 
 	</div>
 </div>
-<!--- Connect -->
-
-<div class="container-fluid padding">
-	<div class="row text-center padding">
-		<!-- lets add the first of the 4 width column -->
-		<div class="col-12">
-			<h2>Connect</h2>
-		</div>
-
-		<div class="col-12 social padding">
-
-			<a href="#"><i class="fab fa-facebook"></i></a>
-			<a href="#"><i class="fab fa-twitter"></i></a>
-			<a href="#"><i class="fab fa-google-plus-g"></i></a>
-			<a href="#"><i class="fab fa-instagram"></i></a>
-			<a href="#"><i class="fab fa-youtube"></i></a>
-												
-			<!-- <a href="facebook.com/"-->
-		</div>
-
-
-	</div>
-</div>
 <!--- Footer -->
 <footer>
 	<div class="container-fluid padding">
