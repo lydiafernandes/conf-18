9ee375227ed4f7e5818a581d5d9149bc5e16688c
lydiafernandes <fdes.lydia@gmail.com>
2018-08-21 23:56:07 +0530

    logo addition

--- includes/navigation1.php
+++ includes/navigation1.php
@@ -1,7 +1,7 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-md bg-dark navbar-dark">
   <a class="navbar-brand" href="#" border="0">
-   <img src="img/sci_conf18_logo.png" height="10%" width="10%" alt="Logo of science conference">
+   <img src="img/sci_conf18_logo.png" alt="Logo of science conference">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
