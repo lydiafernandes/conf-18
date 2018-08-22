2ace888cbd956607b35755462c2f80a2b1bb481f
lydiafernandes <fdes.lydia@gmail.com>
2018-08-22 19:14:42 +0530

    logo dimension adjustment

--- includes/navigation1.php
+++ includes/navigation1.php
@@ -1,7 +1,7 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-md bg-dark navbar-dark">
   <a class="navbar-brand" href="img/logo_black_bgrnd.png" target="_blank" border="0">
-   <img src="img/logo_black_bgrnd.png" alt="Logo of science conference">
+   <img src="img/logo_grey_bgrnd.jpg" alt="Logo of science conference" style="margin:0px;">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
