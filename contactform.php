286424cfca88f95c09c1da15489ea9cf689ce0c1
lydiafernandes <fdes.lydia@gmail.com>
2018-08-28 22:35:28 +0530

    contactform.php

--- contactform.php
+++ contactform.php
@@ -1,14 +1,17 @@
 <?php
+require "phpmailer/PHPMailerAutoload.php";
+
 echo "testing";
-    if(isset($_POST['submit']))
+   if(isset($_POST['submit']))
     {
         echo "data has come in";
         $name = $_POST['frm_name'];
         $email_from = $_POST['frm_email'];
+        $email_to = "isc150@xaviers.edu";
         $subject = $_POST['frm_subject'];
         $msg = $_POST['frm_msg'];
 
-        $mail_to = "isc150@xaviers.edu";
+      /*  $mail_to = "isc150@xaviers.edu";
         $headers = "From: ".$email_from;
         $txt = "You have received an email from ". $name.".\n\n".$msg;
 
@@ -21,6 +24,7 @@ echo "testing";
     {
         echo "Something went wrong";
         //header("Location: contact.php);
-    }
+    }*/
+
 
 ?>
