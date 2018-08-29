796fa0175379e6d1c3231854ebc4175d14e5912c
lydiafernandes <fdes.lydia@gmail.com>
2018-08-29 21:33:18 +0530

    link to fb page

--- includes/footer.php
+++ includes/footer.php
@@ -9,8 +9,8 @@
 
 		<div class="col-12 social padding">
 
-			<a href="#"><i class="fab fa-facebook"></i></a>
-			<a href="#"><i class="fab fa-twitter"></i></a>
+			<a href="https://www.facebook.com/alumnixaviers/"><i class="fab fa-facebook"></i></a>
+			<!-- a href="#"><i class="fab fa-twitter"></i></a>
 			<a href="#"><i class="fab fa-google-plus-g"></i></a>
 			<a href="#"><i class="fab fa-instagram"></i></a>
 			<a href="#"><i class="fab fa-youtube"></i></a>
