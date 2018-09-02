da055b5bfaadc3c4c24b037f1e22a73aa6c0d146
lydiafernandes <fdes.lydia@gmail.com>
2018-09-02 23:47:57 +0530

    rajmohan read more

--- rajmohan.php
+++ rajmohan.php
@@ -8,11 +8,22 @@
          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
+        <script>
+             $(document).ready(function(){
+               $("button").click(function(){
+                $("#speaker").toggle();
+            });
+         });
+        </script>
         <!-- popper.js is recommended for bootstrap -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
-        <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script><script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+        <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+        
+
+        
+
 
         <!-- this file is included in the externa file -->
         <link rel="stylesheet" type="text/css" href="css/style.css"> 
@@ -59,13 +70,7 @@
   }
   
   </style>
-  <script>
-    $(document).ready(function(){
-         $("button").click(function(){
-                $("#speaker").toggle();
-          });
-    });
-</script>
+  
 </head>
 <body> 
     <!-- include the nav bar -->
