9407a7b1d1a85a0aab627bfabfc7c464f7fa1106
lydiafernandes <fdes.lydia@gmail.com>
2018-09-02 23:58:24 +0530

    rajmohan read more

--- rajmohan.php
+++ rajmohan.php
@@ -8,7 +8,15 @@
          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-        <script>
+        
+        <!-- popper.js is recommended for bootstrap -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+        <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+        
+        <script type="text/javascript">
              $(document).ready(function(){
                $("button").click(function(){
                    alert("hi");
@@ -16,20 +24,8 @@
             });
          });
         </script>
-        <!-- popper.js is recommended for bootstrap -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-    	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-
-        <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
-        
-
-        
-
-
-        <!-- this file is included in the externa file -->
-        <link rel="stylesheet" type="text/css" href="css/style.css"> 
-
-
+<!-- this file is included in the externa file -->
+<link rel="stylesheet" type="text/css" href="css/style.css"> 
 <style>
  /* body {
       font: 20px Montserrat, sans-serif;
@@ -41,6 +37,7 @@
   #speaker {
       font-size: 16px;
       display:none;
+      color:yellow;
       }
 
   .margin {margin-bottom: 45px;}
