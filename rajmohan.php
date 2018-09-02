d55774e1f28e00dd21c91fff72807a7792f8603b
lydiafernandes <fdes.lydia@gmail.com>
2018-09-03 00:08:42 +0530

    rajmohan read more

--- rajmohan.php
+++ rajmohan.php
@@ -88,7 +88,7 @@
   <h3 class="margin">
   <button type="button" class="btn btn-outline-light btn-lg">Read more</button>
   </h3>
-    <p>
+    <p style="display:none;">
     Hide me
 </p>
         <p id="speaker" style="display:none;">
