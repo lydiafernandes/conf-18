c298e5917429435aa29e6fb845313a95424300eb
lydiafernandes <fdes.lydia@gmail.com>
2018-09-03 20:01:31 +0530

    org committee page

--- org_com1.php
+++ org_com1.php
@@ -30,7 +30,7 @@
          An Interdisciplinary Approach From Fundamental Sciences to Translational Medicine</h1>
          <div class="container row">
 
-            <div class="tree col-xs-12>
+            <div class="tree col-xs-12">
                 <ul>
 	            	<li>
 		            	<a href="#"><strong>Organizing Committee</strong></a>
