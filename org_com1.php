69f7a696044272b88e6e58d9e7f25673a2cbc6b0
lydiafernandes <fdes.lydia@gmail.com>
2018-09-03 19:34:11 +0530

    org committee page

---
+++ org_com1.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<HTML>
+    <head>
+        <title> Organizing Committee </title>
+        <meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <!-- popper.js is recommended for bootstrap -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+        <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script><script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+        <!-- this file is included in the externa file -->
+        <link rel="stylesheet" type="text/css" href="css/style.css"> 
+        <link type="text/css" href="css/style_orgchart.css" rel="stylesheet"/>
+        
+       
+
+
+</head>
+<body> 
+    <!-- include the nav bar -->
+<?php include("includes/navigation1.php");?>
+
+
+<!-- -------------------------------------------------->
+<h1 style="text-align:center;"> From Health to Well Being
+         An Interdisciplinary Approach From Fundamental Sciences to Translational Medicine</h1>
+<div class="tree">
+    <ul>
+		<li>
+			<a href="#"><strong>Organizing Committee</strong></a>
+			<ul>
+			    
+				<li>
+					<a href="#"><strong>Creighton University / President:</strong> <br> Dr. (Rev.) Daniel Hendrickson, SJ.</a>
+					
+				
+					<ul>
+						<li>
+							<a href="#"><strong>Conference Chairperson / Convener 2:</strong><br> Prof. (Dr.)D. K. Agarwal </a>
+							
+							<ul>
+					        	<li>
+						        	<a href="#"><strong>International Co-ordinator </strong><br> Dr. Finosh Thankam </a>
+					        	</li>
+				        	</ul>
+						</li>
+					</ul>
+				</li>
+				
+				<li>
+					<a href="#"><strong>St. Xavier&apos;s College-Autonomous / Principal: </strong><br> Prof. (Dr.) Rajendra Shinde</a>
+					
+					<ul>
+					    <li>
+					        <a href="#"><strong> Conference / Convener 1: </strong> <br> Dr. (Fr.) Roy Pereira </a>
+					   
+					<ul>
+						
+						<li>
+							<a href="#"><strong>Organizing Secretaries:</strong> <br> Dr. Ashma Aggarwal, <br> Mr. Marazaban Kotwal</a>
+							<ul>
+								<li>
+									<a href="#"><strong> Advisory Board </strong></a>
+								</li>
+								<li>
+									<a href="#"><strong> Scientific Board </strong></a>
+								</li>
+								<li>
+									<a href="#"><strong> Organizing Team </strong></a>
+								</li>
+							</ul>
+						</li>
+						
+					
+					</li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+	</ul>
+</div>
+
+
+<!--- Connect-->
+
+<?php include("includes/footer.php");?>
+</body>
+</html>
