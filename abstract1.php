077708fb034a5e840dbd9d633613e0cb18f05be8
lydiafernandes <fdes.lydia@gmail.com>
2018-09-05 17:04:38 +0530

    registration form

---
+++ abstract1.php
@@ -0,0 +1,391 @@
+
+
+<!DOCTYPE html>
+
+
+<head>
+  <title>Abstract Submission</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  <!-- popper.js is recommended for bootstrap -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <!-- fontawesome will allow us to create icons in tde input sections -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
+
+  <!-- downlading a font awesome for creating an icon in tde login-->
+  <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+
+  <!-- tdis file is included in tde externa file -->
+  <link rel="stylesheet" type="text/css" href="style.css"> 
+
+<script src="main.js"></script>
+</head>
+
+<body>
+	<!--<div class="modal-dialog text-center">-->
+  	<!-- should occupy 2/3rd of tde screen for above 576 px screen sm stands for small screen-->
+  	<!-- main-section is for styling -->
+		<div class="col-sm-8 main-section"> 
+			<div class="modal-content">
+  			
+				<div class="col-12 user-img"> <!-- it will utilize all 12 columns -->
+					<img src="img/logo_college.png">
+				</div>
+				
+				<form class="col-12" method="post" action="register.php">
+				
+				<legend>
+					<p> <font color="white"><h4>Author Details</h4></p>
+				</legend>
+				
+				<table>
+					<tr>
+						<div class="form-group">
+						<input type="text" name="head" class="form-control" placeholder="Title of Abstract" >
+						</div>
+					</tr>
+				</table>
+				<table>
+					<tr>
+						<td>
+							Author 1:
+						</td>
+							
+						
+						<td>
+						
+							<div class="form-group" >
+								<select class="form-control" name="t1" >
+									<option value="" hidden> Designation/Title </option>
+									<option value="Miss">Miss</option>
+									<option value="Mr.">Mr.</option>
+									<option value="Mrs.">Mrs.</option>
+									<option value="Ms.">Ms.</option>
+									<option value="Prof.">Prof.</option>
+									<option value="Dr.">Dr.</option>
+									<option value="Asst. Prof. Dr.">Asst. Prof. Dr.</option>
+									<option value="Assoc. Prof. Dr.">Assoc. Prof. Dr.</option>
+								</select>
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="f1" class="form-control" placeholder="First Name" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="l1" class="form-control" placeholder="Last Name" >
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="u1" class="form-control" placeholder="Company/University" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="c1" class="form-control" placeholder="Country" >
+							</div>
+						</td>
+					
+						<td>
+							<input type="radio" name="c_auth" value="Oral">Corresponding Author	
+						</td>
+					</tr>
+					
+					
+					
+					<tr>
+						<td>
+							Author 2:
+						</td>
+							
+						
+						<td>
+						
+							<div class="form-group" >
+								<select class="form-control" name="t1" >
+									<option value="" hidden> Designation/Title </option>
+									<option value="Miss">Miss</option>
+									<option value="Mr.">Mr.</option>
+									<option value="Mrs.">Mrs.</option>
+									<option value="Ms.">Ms.</option>
+									<option value="Prof.">Prof.</option>
+									<option value="Dr.">Dr.</option>
+									<option value="Asst. Prof. Dr.">Asst. Prof. Dr.</option>
+									<option value="Assoc. Prof. Dr.">Assoc. Prof. Dr.</option>
+								</select>
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="f1" class="form-control" placeholder="First Name" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="l1" class="form-control" placeholder="Last Name" >
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="u1" class="form-control" placeholder="Company/University" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="c1" class="form-control" placeholder="Country" >
+							</div>
+						</td>
+					
+					
+						<td>
+							<input type="radio" name="c_auth" value="Oral">Corresponding Author	
+						</td>
+					</tr>
+					
+					
+					
+					<tr>
+						<td>
+							Author 3:
+						</td>
+							
+						
+						<td>
+						
+							<div class="form-group" >
+								<select class="form-control" name="t1" >
+									<option value="" hidden> Designation/Title </option>
+									<option value="Miss">Miss</option>
+									<option value="Mr.">Mr.</option>
+									<option value="Mrs.">Mrs.</option>
+									<option value="Ms.">Ms.</option>
+									<option value="Prof.">Prof.</option>
+									<option value="Dr.">Dr.</option>
+									<option value="Asst. Prof. Dr.">Asst. Prof. Dr.</option>
+									<option value="Assoc. Prof. Dr.">Assoc. Prof. Dr.</option>
+								</select>
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="f1" class="form-control" placeholder="First Name" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="l1" class="form-control" placeholder="Last Name" >
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="u1" class="form-control" placeholder="Company/University" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="c1" class="form-control" placeholder="Country" >
+							</div>
+						</td>
+					
+						<td>
+							<input type="radio" name="c_auth" value="Oral">Corresponding Author	
+						</td>
+					</tr>
+					
+					
+					
+					<tr>
+						<td>
+							Author 4:
+						</td>
+							
+						
+						<td>
+						
+							<div class="form-group" >
+								<select class="form-control" name="t1" >
+									<option value="" hidden> Designation/Title </option>
+									<option value="Miss">Miss</option>
+									<option value="Mr.">Mr.</option>
+									<option value="Mrs.">Mrs.</option>
+									<option value="Ms.">Ms.</option>
+									<option value="Prof.">Prof.</option>
+									<option value="Dr.">Dr.</option>
+									<option value="Asst. Prof. Dr.">Asst. Prof. Dr.</option>
+									<option value="Assoc. Prof. Dr.">Assoc. Prof. Dr.</option>
+								</select>
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="f1" class="form-control" placeholder="First Name" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="l1" class="form-control" placeholder="Last Name" >
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="u1" class="form-control" placeholder="Company/University" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="c1" class="form-control" placeholder="Country" >
+							</div>
+						</td>
+					
+						<td>
+							<input type="radio" name="c_auth" value="c_auth">Corresponding Author	
+						</td>
+					</tr>
+					
+					
+					
+					<tr>
+						<td>
+							Author 5:
+						</td>
+							
+						
+						<td>
+						
+							<div class="form-group" >
+								<select class="form-control" name="t1" >
+									<option value="" hidden> Designation/Title </option>
+									<option value="Miss">Miss</option>
+									<option value="Mr.">Mr.</option>
+									<option value="Mrs.">Mrs.</option>
+									<option value="Ms.">Ms.</option>
+									<option value="Prof.">Prof.</option>
+									<option value="Dr.">Dr.</option>
+									<option value="Asst. Prof. Dr.">Asst. Prof. Dr.</option>
+									<option value="Assoc. Prof. Dr.">Assoc. Prof. Dr.</option>
+								</select>
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="f1" class="form-control" placeholder="First Name" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="l1" class="form-control" placeholder="Last Name" >
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="u1" class="form-control" placeholder="Company/University" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="text" name="c1" class="form-control" placeholder="Country" >
+							</div>
+						</td>
+				
+						<td>
+							<input type="radio" name="c_auth" value="Oral">Corresponding Author	
+						</td>
+					</tr>
+				</table>
+				<legend>
+				<p> <font color="white">	<h4> Corresponding Author Details: </h4></p>
+				</legend>
+				<table>
+					<tr>
+						<td>
+							<div class="form-group">
+								<input type="email" name="email" class="form-control" placeholder="Email" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+								<input type="phone" name="mobile" class="form-control" placeholder="Phone" >
+							</div>
+						</td>
+					</tr>
+					
+					
+					
+					
+				</table>
+				
+				<table>
+					
+							<button type="submit" class="btn" name="abs_sub">
+								<i class="fas fa-sign-in-alt"> </i> Proceed 
+							</button>
+					
+				</table>				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				</form>
+			</div>
+		</div>
+	<!--</div>-->
+</body>
+</html>
