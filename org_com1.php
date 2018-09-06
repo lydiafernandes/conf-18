fc696a34e3b32f15c66507067c227ddddecb4586
lydiafernandes <fdes.lydia@gmail.com>
2018-09-07 00:23:08 +0530

    org page image modification to 25%

--- org_com1.php
+++ org_com1.php
@@ -75,16 +75,7 @@
 							</a>
 					        <ul>
 					            <li>
-
-								<img src="img/org_com_img/roy_p1.jpg" height="25%" width="20%" 
-								class="img-responsive rounded" style="display:inline" alt="pic of Dr. Rajendra Shinde">
-								<br>
-								<a href="#">
-									Prof. (Dr.) Rajendra Shinde
-								<br>
-								<strong>St. Xavier&apos;s College-Autonomous / Principal </strong>
 								
-							</a>
 					                <a href="#"><strong> Conference / Convener 1: </strong> <br> Dr. (Fr.) Roy Pereira </a>
 					   
 					                <ul>		
