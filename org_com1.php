78ec55c18eba4087b3a0cc96b51ae82ebc80f14e
lydiafernandes <fdes.lydia@gmail.com>
2018-09-07 00:29:45 +0530

    org page image modification to 25%

--- org_com1.php
+++ org_com1.php
@@ -60,7 +60,7 @@
 				
 			        	<li>
 					        <a href="#">
-								<img src="img/org_com_img/Principal_sxc.jpg" height="12%" width="12%" 
+								<img src="img/org_com_img/Principal_sxc1.png" height="12%" width="12%" 
 								class="img-responsive rounded" style="display:inline" alt="pic of Dr. Rajendra Shinde">
 								<br>
 									Prof. (Dr.) Rajendra Shinde
