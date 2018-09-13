a90b64ae74de778f71610c5f389d29e02adc4d17
lydiafernandes <fdes.lydia@gmail.com>
2018-09-13 21:49:15 +0530

    org com 2.php

---
+++ org_com2.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<HTML>
+    <head>
+        <title> Contact Us </title>
+        <meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <!-- popper.js is recommended for bootstrap -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+        <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script><script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+        <!-- this file is included in the externa file -->
+        <link rel="stylesheet" type="text/css" href="css/style.css"> 
+<style>
+        	.pad_center{
+        		text-align: center;
+        	    padding:10px;
+        	    border-radius:5px;
+        	    border-color:#000000;
+        	    border:1px;
+        	    border-style: solid;
+        	    font-family: quicksand;
+        	    color:#ffffff;
+        	}
+
+        	h2:hover,h3:hover{
+        		background-color: #000000;
+        		color:#ffffff;
+        		border-color: #ffffff;
+        		cursor: -webkit-grabbing; cursor: grabbing;
+        	}
+
+        	/*.grabbing {cursor: -webkit-grabbing; cursor: grabbing;}*/
+        	a{
+        		color:#ffffff;
+        	}
+
+        	 a.names:link, .names:visited, .names:hover, .names:active {
+        		color:white;
+        	}
+    	</style>
+                
+       
+
+
+</head>
+<body> 
+    <!-- include the nav bar -->
+<?php include("includes/navigation1.php");?>
+
+
+<!-- -------------------------------------------------->
+	<div class="container-fluid">
+		<h1> Conference Team </h1>
+<!-- row 1 begins -->
+		<div class="row">
+			<div class="col-sm-4" style="background-color:#cbcdd9;"> &nbsp; </div>	
+			<!-- <div class="col-sm-4" style="background-color:#cbcdd9;"> &nbsp; </div> -->	
+			<div class="col-sm-4 pad_center" style="background-color:#125772;"> <h2><span>Patrons</span></h2> </div>	
+			<div class="col-sm-4" style="background-color:#cbcdd9;"> &nbsp; </div>	
+			<!-- <div class="col-sm-3" style="background-color:#cbcdd9;"> &nbsp; </div> -->	
+		</div> <!-- end of div row 1 -->
+		
+		<!-- row 2 begins -->
+		<div class="row">
+    		<div class="col-sm-3" style="background-color:#cbcdd9;">&nbsp;</div>
+   		 	<div class="col-sm-3 pad_center" style="background-color:#B3A97D;">
+   		 		<span> 
+   		 			<a href="#"><strong>Creighton University / President:</strong> <br>	Dr. (Rev.) Daniel Hendrickson, SJ.</a>
+				</span>
+				
+   		 	</div>
+   		 	<div class="col-sm-3 pad_center" style="background-color:#B3A97D;">
+   		 		<span>
+   		 		<a href="#"><strong>St. Xavier&apos;s College / Rector:</strong> <br>
+							Fr. Keith Dsouza, SJ.</a>
+				</span>
+			</div>
+   		 	<div class="col-sm-3" style="background-color:#cbcdd9;">&nbsp;</div>
+		</div> <!-- end of div row 2 -->
+
+		<!-- row 3 begins -->
+		<div class="row">
+			<div class="col-sm-12" style="background-color:#cbcdd9; padding:5px;"> &nbsp; </div>	
+		</div><!-- end of div row 3 -->
+
+		<!-- row 4 begins -->
+		<div class="row">
+			<div class="col-sm-4" style="background-color:#cbcdd9;"> &nbsp; </div>	
+			<!-- <div class="col-sm-2" style="background-color:#cbcdd9;"> &nbsp; </div> -->	
+			<div class="col-sm-4 pad_center" style="background-color:#ff9962;"> 
+				<p><strong> <u><i>Chairperson</i></u> </strong> <br>
+				<big>Prof. (Dr.) Rajendra Shinde</big>
+				<br><a href="#">
+						<strong>St. Xavier&apos;s College-Autonomous / Principal </strong>
+					</a>
+				</p>
+			</div>	
+			<div class="col-sm-4" style="background-color:#cbcdd9;"> &nbsp; </div>	
+			<!-- <div class="col-sm-3" style="background-color:#cbcdd9;"> &nbsp; </div> -->	
+		</div> <!-- end of div row 4 -->
+
+		<!-- row 5 begins -->
+		<div class="row">
+			<div class="col-sm-12" style="background-color:#cbcdd9; padding:5px;"> &nbsp; </div>	
+		</div><!-- end of div row 5 -->
+
+		<!-- row 6 begins -->
+		<div class="row">
+			<div class="col-sm-4" style="background-color:#cbcdd9;"> &nbsp; </div>	
+			<!-- <div class="col-sm-4" style="background-color:#cbcdd9;"> &nbsp; </div> -->	
+			<div class="col-sm-4 pad_center" style="background-color:#125772;"> <h3><span>Conveners </span></h3></div>	
+			<div class="col-sm-4" style="background-color:#cbcdd9;"> &nbsp; </div>	
+			<!-- <div class="col-sm-3" style="background-color:#cbcdd9;"> &nbsp; </div> -->	
+		</div> <!-- end of div row 6 -->
+
+				<!-- row 7 begins -->
+		<div class="row">
+    		<div class="col-sm-2" style="background-color:#cbcdd9;">&nbsp;</div>
+   		 	<div class="col-sm-4 pad_center" style="background-color:#506F82;">
+   		 		<span> 
+   		 			<p><a class="names" href="#">Prof. (Dr.) D. K. Agarwal <br> 
+   		 				<strong>	Creighton University</strong></a></p>
+				</span>
+				
+   		 	</div>
+   		 	<div class="col-sm-4 pad_center" style="background-color:#506F82;">
+   		 		<span>
+   		 		 <p> <a class="names" href="#"> Dr. (Fr.) Roy Pereira  <br><strong>St. Xavier&apos;s College-Autonomous / Vice Principal </strong>  </a></p>
+   		 		</span>
+			</div>
+   		 	<div class="col-sm-2" style="background-color:#cbcdd9;">&nbsp;</div>
+		</div> <!-- end of div row 7 -->
+
+		<!-- row 8 begins -->
+		<div class="row">
+			<div class="col-sm-12" style="background-color:#cbcdd9; padding:5px;"> &nbsp; </div>	
+		</div><!-- end of div row 8 -->
+       
+
+        <!-- row 9 begins -->
+		<div class="row">
+    		<div class="col-sm-1" style="background-color:#cbcdd9;">&nbsp;</div>
+    		<div class="col-sm-2 pad_center" style="background-color:#9d4672;">
+
+    			 <h6><span> <u><i>International Co-ordinator:</i></u>
+					<br> Dr. Finosh Thankam </span></h6>
+    		</div>
+   		 	<div class="col-sm-2" style="background-color:#cbcdd9;">&nbsp;</div>
+   		 	<div class="col-sm-2 pad_center" style="background-color:#230A52;">
+   		 		
+   		 		<h6><span><u><i>Organising Secretaries</i></u>
+   		 		  <br> Dr. Ashma Aggarwal <br> Mr. Marazaban Kotwal </span></h6>
+   		 	</div>
+   		 	<div class="col-sm-2" style="background-color:#cbcdd9;">&nbsp;</div>
+   		 	<div class="col-sm-2 pad_center" style="background-color:#0181c7;">
+
+   		 		<h6><span><u><i>Technical Committee</i></u></span></h6>
+   		 	</div>
+   		 	<div class="col-sm-1" style="background-color:#cbcdd9;">&nbsp;</div>
+		</div> <!-- end of div row 9 -->
+	</div>
+
+
+<!--- Connect-->
+
+<?php include("includes/footer.php");?>
+</body>
+</html>
