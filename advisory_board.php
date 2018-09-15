2b52cfd91d2005c0bb82e927a9ea8167ba7a01b1
lydiafernandes <fdes.lydia@gmail.com>
2018-09-16 02:41:34 +0530

    advisory board

--- advisory_board.php
+++ advisory_board.php
@@ -30,6 +30,59 @@
 
 
 <!-- -------------------------------------------------->
+<ul class="faq">
+
+    <li class="q">
+        <img src="img/arrow.png"> 
+        Dr. N.K.Ganguly
+    </li>
+    <li class="a">
+    Prof Nirmal K. Ganguly is Advisor, Translational Health Science and Technology Institute and President, 
+    Jawaharlal Institute of Post Graduate Medical Education and Research in India. He is a former Director General of the Indian Council of Medical Research.     
+    <a href="http://www.who.int/neglected_diseases/ganguly/en/" class="btn btn-outline-secondary">See Profile</a>
+    </li>
+
+<!-- -------------End of Q1------------------------------ -->
+
+    <li class="q">
+        <img src="img/arrow.png"> 
+        Dr. Vibha Tandon (Jawaharlal Nehru University, New Delhi)
+    </li>
+    <li class="a">
+    Professor  &amp; Chairperson Special Centre for Molecular Medicine. <br> With 22 years of research experience, Dr. Tandon specializes in Chemical Biology ,Radiation Biology 
+    &amp; Cell Signalling, Drug Development and Study of their mechanism of Action based on Genomics &amp;
+     Proteomics, Development of Antibiotics for Gyrase Resistant Strains targeting Topoisomerase IA 
+     Gene in bacteria.    
+    <a href="https://www.jnu.ac.in/content/vtandon" class="btn btn-outline-secondary">See Profile</a>
+    </li>
+
+<!-- -------------End of Q2------------------------------ -->
+<li class="q">
+        <img src="img/arrow.png"> 
+        Dr. Nishigandha Naik (Haffkine Institute, Mumbai)
+    </li>
+    <li class="a">Director in Charge <br>
+    Nishigandha Naik is working as a Director (additional charge) at the Haffkine Institute for Training,
+    Research and Testing (HITRT), Mumbai and is the Head of the Dept. of Biochemistry and Dept. of 
+    Toxicology. She has more than twenty eight years of research work experience that includes academia 
+    as well as industry. Her work is in the field of cancer cell biology, imaging and pre-clinical drug
+     discovery. Her basic research is focused on cell motility related signal transduction events and
+      development of therapeutic approaches for cancer, especially chronic myeloid leukemia, breast
+       cancer, glioblastoma and neural stem cells.
+        
+    <a href="https://www.researchgate.net/profile/Nishigandha_Naik" class="btn btn-outline-secondary">See Profile</a>
+    </li>
+
+<!-- -------------End of Q3------------------------------ -->
+<!--li class="q">
+        <img src="img/arrow.png"> 
+    
+    </li>
+    <li class="a"><a href="" class="btn btn-outline-secondary">See Profile</a>
+    </li>
+
+<!-- -------------End of Q4------------------------------ -->
+</ul>
 
 <!--- Connect-->
 
