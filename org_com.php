92b39c94f613dbc8256ddbb3c1599d0c2b8edff0
lydiafernandes <fdes.lydia@gmail.com>
2018-09-16 23:35:46 +0530

    org com

--- org_com.php
+++ org_com.php
@@ -142,11 +142,7 @@
     		<div class="col-sm-2" style="background-color:#cbcdd9;">&nbsp;</div>
    		 	<div class="col-sm-8 pad_center" style="background-color:#506F82;">
 				<span>
-   		 		<span <span class="sec_box" style="float:left;">
-                    <img src="img\org_com_img\org_dk_agarwal.png" class="img-thumbnail" alt="pic of Prof. (Dr.) D. K. Agarwal" width="100px" height="200px"><br> 
-   		 			<a class="names" href="#">Prof. (Dr.) D. K. Agarwal <br> 
-   		 				<strong>	Creighton University / Professor </strong></a>
-				</span>
+   		 		
 				
    		 	<!--/div>
    		 	<div class="col-sm-4 pad_center" style="background-color:#506F82;"-->
@@ -154,6 +150,12 @@
                     <img src="img\org_com_img\roy_p1.png" class="img-thumbnail" alt="pic of Dr. (Fr.) Roy Pereira" width="100px" height="200px"><br>
                     <a class="names" href="#"> Dr. (Fr.) Roy Pereira  <br><strong>St. Xavier&apos;s College-Autonomous / Vice Principal </strong>  </a>
    		 		</span>
+
+					<span <span class="sec_box" style="float:left;">
+                    <img src="img\org_com_img\org_dk_agarwal.png" class="img-thumbnail" alt="pic of Prof. (Dr.) D. K. Agarwal" width="100px" height="200px"><br> 
+   		 			<a class="names" href="#">Prof. (Dr.) D. K. Agarwal <br> 
+   		 				<strong>	Creighton University / Professor </strong></a>
+				</span>
 
 					</span>
 			</div>
