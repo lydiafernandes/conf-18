3fe357c42fba23cbfa6cc0ebc4748240c6d031f2
lydiafernandes <fdes.lydia@gmail.com>
2018-09-20 15:45:48 +0530

    column1

--- org_com2.php
+++ org_com2.php
@@ -210,44 +210,7 @@
 		</div> <!-- end of div row 9 -->
 
             <!-- ---------------------- row 9.0 ends ------------------------------ -->
-        <!-- row 9 begins -->
-		<div class="row">
-    		<div class="col-sm-2" style="background-color:#cbcdd9;">&nbsp;</div>
-    		
-   		 	<div class="col-sm-2 pad_center" style="background-color:#230A52;">
-   		 		        
-                   
-                        <img src="img\org_com_img\ash_agg.jpg" class="img-thumbnail" alt="pic of Dr. Ashma Aggarwal" width="100px" height="200px">
-                        <span> 
-                        Dr. Ashma Aggarwal <br><u><i>Organising Secretary</i><u><br> St. Xavier&apos;s  College  / Faculty 
-                        </span>
-                  
-
-            </div>
-
-            <div class="col-sm-2" style="background-color:#cbcdd9;">&nbsp;</div>
-
-            <div class="col-sm-2 pad_center">
-                    
-                      <img src="img\org_com_img\finosh_t.jpg"  class="img-thumbnail"  alt="pic of Dr. Finosh Thankam" width="100px" height="200px"><br>
-                      <span>
-                       Dr. Finosh Thankam <br> <u><i>International Co-ordinator:</i></u><br> <strong>Creighton University</strong 
-                       </span>
-					
-            </div>
-
-            <div class="col-sm-2" style="background-color:#cbcdd9;">&nbsp;</div>
-
-             <div class="col-sm-2 pad_center">
-            
-                        <img src="img\org_com_img\m_k.jpg" class="img-thumbnail" alt="pic of Marzban Kotwal" width="100px" height="200px">
-                        <!-- br><br>Mr. Marzban Kotwal <br><u><i> Organising Secretary </i></u><br>St. Xavier&apos;s  College  / Faculty -->
-        	</div>
-
-            <div class="col-sm-2" style="background-color:#cbcdd9;">&nbsp;</div>
-           
-		</div> <!-- end of div row 9 -->
-
+       
 	</div>
 
 
