b12a612e8a9a17a2ea331a4e20169e9bf3191ebd
lydiafernandes <fdes.lydia@gmail.com>
2018-09-20 15:41:57 +0530

    column1

--- org_com2.php
+++ org_com2.php
@@ -167,29 +167,47 @@
 			<div class="col-sm-12" style="background-color:#cbcdd9; padding:5px;"> &nbsp; </div>	
 		</div><!-- end of div row 8 -->
        <!-- ---------------------------row 9.0 --------------------------- -->
-        <div class="row">
+       <!-- row 9 begins -->
+		<div class="row">
+    		<div class="col-sm-1" style="background-color:#cbcdd9;">&nbsp;</div>
+    		
+   		 	<div class="col-sm-4 pad_center" style="background-color:#230A52;">
+   		 		
+   		 		
+                    <span>
+                   
+                        <span class="sec_box" style="float:left;">
+                             <img src="img\org_com_img\ash_agg.jpg" class="img-thumbnail" alt="pic of Dr. Ashma Aggarwal" width="100px" height="200px">
+                            <br><br> Dr. Ashma Aggarwal 
+                         </span> 
+                        <span class="sec_box" style="float:right;">
+                            <img src="img\org_com_img\m_k.jpg" class="img-thumbnail" alt="pic of Marzban Kotwal" width="100px" height="200px">
+                             <br><br>Mr. Marzban Kotwal 
+                        </span><br>
+						<p style="float:left; text-align:center;"><br>
+						<u><i>&nbsp;&nbsp;&nbsp;Organizing Secretaries:</i></u><br><strong>&nbsp;&nbsp;&nbsp;St. Xavier&apos;s  College  / Faculty</strong>
+                        </p>
+                    </span>
+   		 	</div>
 
-            <div class="col-sm-4" style="background-color:lavender;">
-            <img src="img\org_com_img\ash_agg.jpg" class="img-thumbnail" alt="pic of Dr. Ashma Aggarwal" width="100px" height="200px">
-             <p> 
-                        Dr. Ashma Aggarwal <br><u><i>Organising Secretary</i><u><br> St. Xavier&apos;s  College  / Faculty 
-            </p>
-            </div>
+				<div class="col-sm-2" style="background-color:#cbcdd9;">&nbsp;</div>
+ 
+   		 	
+				<div class="col-sm-4 pad_center" style="background-color:#9d4672;">
 
-            <div class="col-sm-4" style="background-color:lavenderblush;">
-            <img src="img\org_com_img\ash_agg.jpg" class="img-thumbnail" alt="pic of Dr. Ashma Aggarwal" width="100px" height="200px">
-             <p> 
-                        Dr. Ashma Aggarwal <br><u><i>Organising Secretary</i><u><br> St. Xavier&apos;s  College  / Faculty 
-            </p>
-            </div>
+    			 <h6>
+                 <span>
+                 <img src="img\org_com_img\finosh_t.jpg" class="img-thumbnail" alt="pic of Dr. Finosh Thankam" width="100px" height="200px"><br>
+                 <br> Dr. Finosh Thankam <br><br>
+				 <br> <u><i>International Co-ordinator:</i></u><br> <strong>Creighton University</strong>
+					
+                </span>
+                </h6>
+    		</div>
 
-            <div class="col-sm-4" style="background-color:lavender;">
-            <img src="img\org_com_img\ash_agg.jpg" class="img-thumbnail" alt="pic of Dr. Ashma Aggarwal" width="100px" height="200px">
-             <p> 
-                        Dr. Ashma Aggarwal <br><u><i>Organising Secretary</i><u><br> St. Xavier&apos;s  College  / Faculty 
-            </p>
-            </div>
-        </div>
+			<div class="col-sm-1" style="background-color:#cbcdd9;">&nbsp;</div>
+   		 	
+		</div> <!-- end of div row 9 -->
 
             <!-- ---------------------- row 9.0 ends ------------------------------ -->
         <!-- row 9 begins -->
