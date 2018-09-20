b96b9d3ad9c5f8bf24e6af6271588842ad81cfd2
lydiafernandes <fdes.lydia@gmail.com>
2018-09-20 12:20:02 +0530

    column1

--- org_com2.php
+++ org_com2.php
@@ -170,9 +170,9 @@
 
         <!-- row 9 begins -->
 		<div class="row">
-    		<div class="col-sm-3" style="background-color:#cbcdd9;">&nbsp;</div>
+    		<!--div class="col-sm-3" style="background-color:#cbcdd9;">&nbsp;</div -->
     		
-   		 	<div class="col-sm-2 pad_center" style="background-color:#230A52;">
+   		 	<div class="col-sm-4 pad_center" style="background-color:#230A52;">
    		 		        
                     <span class="sec_box" style="">
                         <img src="img\org_com_img\ash_agg.jpg" class="img-thumbnail" alt="pic of Dr. Ashma Aggarwal" width="100px" height="200px">
@@ -181,7 +181,7 @@
 
             </div>
 
-            <div class="col-sm-2 pad_center" style="background-color:#230A52;">
+            <div class="col-sm-4 pad_center" style="background-color:#230A52;">
                     <span>
                       <img src="img\org_com_img\finosh_t.jpg" style="" class="img-thumbnail" alt="pic of Dr. Finosh Thankam" width="100px" height="200px"><br>
                         <br> Dr. Finosh Thankam <br> <u><i>International Co-ordinator:</i></u><br> <strong>Creighton University</strong>
@@ -189,7 +189,7 @@
                     </span>
             </div>
 
-             <div class="col-sm-2 pad_center" style="background-color:#230A52;">
+             <div class="col-sm-4 pad_center" style="background-color:#230A52;">
                     <span class="sec_box" style="">
                         <img src="img\org_com_img\m_k.jpg" class="img-thumbnail" alt="pic of Marzban Kotwal" width="100px" height="200px">
                         <br><br>Mr. Marzban Kotwal <br><u><i> Organising Secretary </i></u><br>St. Xavier&apos;s  College  / Faculty
@@ -200,7 +200,7 @@
                         </p -->
         	</div>
 
-            <div class="col-sm-3" style="background-color:#cbcdd9;">&nbsp;</div>
+            <!-- div class="col-sm-3" style="background-color:#cbcdd9;">&nbsp;</div -->
    		 	
 				<!--div class="col-sm-4 pad_center" style="background-color:#9d4672;">
 
