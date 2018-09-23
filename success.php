86eb024f493ed9e2d03427697c31345b33044739
lydiafernandes <fdes.lydia@gmail.com>
2018-09-24 01:07:14 +0530

    new changes

--- success.php
+++ success.php
@@ -29,12 +29,15 @@
 <!-- -------------------------------------------------->
 <div class="container-fluid">
     <div class="row">
-        <div class="col-sm-12">
+
+        <div class="col-sm-2">  &nbsp; </div>
+        <div class="col-sm-8">
             <h4> 
                 Congratulations, you have successfully registered for the conference. 
                 <br>You may now move on to the payment process.
             </h4>
         <div>
+        <div class="col-sm-2">  &nbsp; </div>
     </div>
 </div>
 <!--- Connect-->
