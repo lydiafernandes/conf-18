fc315e804e75b7cb57d04fd37af6788cdbaf20f8
lydiafernandes <fdes.lydia@gmail.com>
2018-09-24 02:15:51 +0530

    title change

---
+++ database/delete_regis.php
@@ -0,0 +1,17 @@
+<?php
+
+    require '../dbh.php';
+
+    echo "deleting record";
+
+   // $query = 'DROP TABLE tbl_registrations';
+
+   $query = 'DELETE FROM tbl_registrations where id=5';
+
+    if($conn->query($query)){
+        echo "record deleted";
+    }
+    else echo "problem with query";
+    
+
+               ?>
