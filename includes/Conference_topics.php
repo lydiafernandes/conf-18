82127728da37624f74c9337e5cbfa27ac0323c9c
lydiafernandes <fdes.lydia@gmail.com>
2018-09-30 12:04:43 +0530

    index page

--- includes/Conference_topics.php
+++ includes/Conference_topics.php
@@ -3,14 +3,30 @@
   <h2>Conference Topics and Related Areas</h2>
    <div class="row">
 
-    <div class="col-sm-3"> &nbsp; </div>
-    <div class="col-sm-6"> &nbsp; </div>
-  <table class="table table-hover">
-    <thead>
-      <tr style="background-color:black; color:white;">       
-            <th>Topics</th>      
-     </tr>
-    </thead>
+    <div class="col-sm-6"> 
+    <h2> If you attend the X150 Conference.....</h2>
+			<p> 
+			This conference aims to provide a premier international forum bringing many eminent international and national scientists 
+			with diverse expertise under one platform to highlight unique approaches and present recent 
+			novel findings at the fundamental as well as biological, cellular, and molecular level to 
+			address core questions in the pathophysiology of a disease process. 
+			</p>
+			<p>
+
+			A major
+			 focus will be on a translational approach for a rapid and smooth interface between preclinical and clinical specialties/professionals. Thus, 
+			 the conference comprises of talks, formal panel discussions, interactions with 
+			 St. Xavier’s students and faculty, poster sessions including social events in Mumbai, India.		
+			</p>
+			
+     </div>
+    <div class="col-sm-6">
+      <table class="table table-hover table-striped">
+       <thead>
+             <tr style="background-color:black; color:white;">       
+                <th>Topics</th>      
+             </tr>
+      </thead>
     <tbody>
         <ol>
       <tr>
@@ -80,7 +96,7 @@
     </ol>
     </tbody>
   </table>
-  <div class="col-sm-3"> &nbsp; </div>
+  </div>
 </div> <!-- -------------------------End of div row ----------------- -->
 
 </div> <!-- --------------------------End of div container --------- -->
