1b2a5ce5d78975ac85f556980ecba08b95451998
lydiafernandes <fdes.lydia@gmail.com>
2018-09-30 12:18:29 +0530

    index page

--- includes/Conference_topics.php
+++ includes/Conference_topics.php
@@ -3,22 +3,8 @@
   <h2>Conference Topics and Related Areas</h2>
    <div class="row">
 
-    <div class="col-sm-6"> 
-    <h2> If you attend the X150 Conference.....</h2>
-			<p> 
-			This conference aims to provide a premier international forum bringing many eminent international and national scientists 
-			with diverse expertise under one platform to highlight unique approaches and present recent 
-			novel findings at the fundamental as well as biological, cellular, and molecular level to 
-			address core questions in the pathophysiology of a disease process. 
-			</p>
-			<p>
-
-			A major
-			 focus will be on a translational approach for a rapid and smooth interface between preclinical and clinical specialties/professionals. Thus, 
-			 the conference comprises of talks, formal panel discussions, interactions with 
-			 St. Xavier’s students and faculty, poster sessions including social events in Mumbai, India.		
-			</p>
-			
+    <div class="col-sm-3"> 
+    &nbsp;
      </div>
     <div class="col-sm-6">
       <table class="table table-hover table-striped">
@@ -31,72 +17,57 @@
         <ol>
       <tr>
         <td> <li> Nano science and Nano-technology </li> </td>
-      </tr>
-      <tr>
         <td> <li> Photo-Chemistry and Phyto-medicine </li> </td>
       </tr>
       <tr>
         <td> <li> Green Chemistry </li> </td>
-      </tr>
-      
-      <tr>
         <td> <li> Optical Instruments  in Diagnostic Tools </li> </td>
       </tr>
       <tr>
         <td> <li> Electro analytical Techniques  </li> </td>
-      </tr>
-      <tr>
         <td> <li> Advanced Analytical Techniques </li> </td>
       </tr>
       <tr>
         <td> <li> Translational Medicine </li> </td>
-      </tr>
-      <tr>
         <td> <li> Biotechnology </li> </td>
       </tr>
       <tr>
         <td> <li> Microbiology   </li> </td>
-      </tr>
-      <tr>
-        <td> <li> Cell-Biology   </li> </td>
+         <td> <li> Cell-Biology   </li> </td>
       </tr>
       <tr>
         <td> <li> Alternative Medicine( Ayurveda/ Yunani/ Homeopathic) </li> </td>
-      </tr>
-      <tr>
         <td> <li> Nuclear Medicine  </li> </td>
       </tr>
       <tr>
         <td> <li> Bio-statistics  </li> </td>
-      </tr>
-      <tr>
-        <td> <li> Bio-molecules  </li> </td>
+         <td> <li> Bio-molecules  </li> </td>
       </tr>
       <tr>
         <td> <li> Toxicology  </li> </td>
-      </tr>
-      <tr>
-        <td> <li> Pharmaceuticals </li> </td>
+         <td> <li> Pharmaceuticals </li> </td>
       </tr>
       <tr>
         <td> <li> Clinical Research </li> </td>
-      </tr>
-      <tr>
-        <td> <li> Natural Products in Medicine </li> </td>
+         <td> <li> Natural Products in Medicine </li> </td>
       </tr>
       <tr>
         <td> <li> Bio-physical Chemistry </li> </td>
-      </tr>
-      <tr>
-        <td> <li> Bio-inorganic Chemistry </li> </td>
+         <td> <li> Bio-inorganic Chemistry </li> </td>
       </tr>
       <tr>
         <td> <li> Organometallic complexes in Medicine </li> </td>
+        <td> &nbsp; </td>
+
       </tr>
     </ol>
     </tbody>
   </table>
   </div>
+
+    <div class="col-sm-3"> 
+    &nbsp;
+     </div>
 </div> <!-- -------------------------End of div row ----------------- -->
 
 </div> <!-- --------------------------End of div container --------- -->
