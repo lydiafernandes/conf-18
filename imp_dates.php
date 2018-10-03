fca8e6f1c6ec1adab71a362b61b38a31fa4c2976
lydiafernandes <fdes.lydia@gmail.com>
2018-10-03 13:12:34 +0530

    important dates

---
+++ imp_dates.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<HTML>
+    <head>
+        <title> Contact Us </title>
+        <meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <!-- popper.js is recommended for bootstrap -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+        <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script><script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+        <!-- this file is included in the externa file -->
+        <link rel="stylesheet" type="text/css" href="css/style.css"> 
+        
+       
+
+
+</head>
+<body> 
+    <!-- include the nav bar -->
+<?php include("includes/navigation1.php");?>
+
+
+<!-- -------------------------------------------------->
+<div class="container-fluid">
+  <h2>Three Unequal Columns</h2>
+  <p>To create unequal columns, you have to use numbers. The following example will create a 25%/50%/25% split:</p>
+  <div class="row">
+    <div class="col-3 bg-success">&nbsp;</div>
+    <div class="col-6 bg-warning">.col-6</div>
+    <div class="col-3 bg-success">.col-3</div>
+  </div>
+</div>
+
+<!--- Connect-->
+
+<?php include("includes/footer.php");?>
+</body>
+</html>
