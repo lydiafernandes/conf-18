99b8264814f0bba27f745a4cfe98c4e9c2e9ebd6
lydiafernandes <fdes.lydia@gmail.com>
2018-10-12 21:57:58 +0530

    registration fees

--- registration_fees.php
+++ registration_fees.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        
          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
         <!-- popper.js is recommended for bootstrap -->
@@ -13,6 +13,7 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
         <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>   
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
 
          <!-- this file is included in the externa file -->
