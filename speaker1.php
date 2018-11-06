61944ff2ad134381138f2426f2882e81d2d203dc
lydiafernandes <fdes.lydia@gmail.com>
2018-11-06 18:31:30 +0530

    speaker version1

--- speaker1.php
+++ speaker1.php
@@ -92,6 +92,349 @@
 
 	<!-- ROW 1 ends ---------------------------------------->
 
+	<!-- ROW 2 BEGINS ---------------------------------------->
+
+	<div class="row padding">
+
+		<!-- ROW 2-FIRST CARD APPEARS HERE -->
+		<div class="col-md-4">
+			<div class="card">
+				<img class="card-img-top" src="img/speakersnewdimensions/Luis_Lima_New.png">
+				<div class="card-body">
+					<h4 class="card-title"> Dr. Melchior Luiz Lima M.D. MSc. Ph.D. </h4>
+					<p class="card-text"> A Professor of Cardiovascular Institute “São Francisco de Assis”
+					and Cardiovascular Surgeon, Center of Cardiovascular Surgery &amp; Heart
+				    Transplantation, Meridional Hospital, Cariacica, ES, Brazil. </p>
+					<a href="lima.php" class="btn btn-outline-secondary">See Profile</a>
+				</div>
+			</div>
+		</div>
+
+		<!-- ROW 2 - SECOND CARD APPEARS HERE -->
+		<div class="col-md-4">
+			<div class="card">
+				<img class="card-img-top" src="img/speakersnewdimensions/Subburaman_Mohan_New.png">
+				<div class="card-body">
+					<h4 class="card-title"> Dr. Subburaman Mohan. </h4>
+					<p class="card-text"> Senior Research Career Scientist, Veterans Administration (VA). Director, Musculoskeletal Disease Center, VA Loma Linda, California</p>
+					<a href="mohan.php" class="btn btn-outline-secondary">See Profile</a>
+				</div>
+			</div>
+		</div>
+
+		<!--ROW 2- THIRD CARD APPEARS HERE -->
+		<div class="col-md-4">
+			<div class="card">
+				<img class="card-img-top" src="img/speakersnewdimensions/Alex_New.png">
+				<div class="card-body">
+					<h4 class="card-title"> Dr. Alex C Varghese </h4>
+					<p class="card-text"> Scientific  Director <br>
+                                           ASTRA Fertility Group,<br>
+                                           <b>Web:</b> 
+                                           <a href="www.lifeinvitro.com"> www.lifeinvitro.com</a>
+
+                    </p>
+					<a href="alex.php" class="btn btn-outline-secondary">See Profile</a>
+				</div>
+			</div>
+		</div>
+
+	</div>
+    <!-- ROW 2 ENDS ---------------------------------------->
+
+<!--------------------- ROW 3 BEGINS ------------ -->
+	<div class="row padding">
+
+		<!-- ROW 3-FIRST CARD APPEARS HERE -->
+			<div class="col-md-4">
+				<div class="card">
+					<img class="card-img-top" src="img/speakersnewdimensions/Sorab_New.png">
+					<div class="card-body">
+						<h4 class="card-title"> Dr. Sorab Nariman Dalal.</h4>
+						<p class="card-text"> Scientific Officer ‘G’, Associate Professor - HBNI, Tata Memorial Centre</p>
+						<a href="sorab.php" class="btn btn-outline-secondary">See Profile</a>
+					</div>
+				</div>
+			</div>
+
+		<!-- ROW 3 - SECOND CARD APPEARS HERE -->
+			<div class="col-md-4">
+				<div class="card">
+				<img class="card-img-top" src="img/speakersnewdimensions/Renjith_New.png">
+					<div class="card-body">
+						<h4 class="card-title"> Dr. Renjith P Nair, PhD </h4>
+						<p class="card-text"> Scientist, Sree Sree Chitra Tirunal Institute for Medical Science and Technology Thiruvananthapuram, India.</p>
+						<a href="nair.php" class="btn btn-outline-secondary">See Profile</a>
+					</div>
+				</div>
+			</div>
+
+		<!--ROW 3- THIRD CARD APPEARS HERE -->
+		
+			<div class="col-md-4">
+				<div class="card">
+					<img class="card-img-top" src="img/speakersnewdimensions/Rajmohan_New.png">
+					<div class="card-body">
+						<h4 class="card-title"> Dr. Rajmohan V.</h4>
+						<p class="card-text"> Asst. Professor with a Bachelors in Ayurvedic Medicine and Surgery (BAMS) 
+						and Doctor of Medicine (MD) in Ayurveda from Government Ayurveda College</p>
+						<a href="rajmohan.php" class="btn btn-outline-secondary">See Profile</a>
+					</div>
+				</div>
+			</div>
+
+	</div>
+
+<!-- ROW 3 ENDS ---------------------------------------->
+
+<!-- ROW 4 BEGINS -------------------------------------------------->
+	<div class="row padding">
+
+			<!-- ROW -4 -FIRST CARD APPEARS HERE -->
+			<div class="col-md-4">
+				<div class="card">
+					<img class="card-img-top" src="img/speakersnewdimensions/Praveen_Vemula_New.png">
+					<div class="card-body">
+						<h4 class="card-title"> Dr. Praveen Vemula Ph.D.</h4>
+						<p class="card-text"> Faculty at the Institute for Stem Cell Biology and Regenerative Medicine(inStem), Bangalore, India.</p>
+						<a href="vemula.php" class="btn btn-outline-secondary">See Profile</a>
+					</div>
+			    </div>
+			</div>
+		
+			<!-- ROW 4 - SECOND CARD APPEARS HERE -->
+			<div class="col-md-4">
+				<div class="card">
+					<img class="card-img-top" src="img/speakersnewdimensions/Oormila_New.png">
+					<div class="card-body">
+						<h4 class="card-title"> Dr. Oormila Kovilam </h4>
+						<p class="card-text"> Amita Health, Chicago Illinois, USA</p>
+						<a href="kovilam.php" class="btn btn-outline-secondary">See Profile</a>
+					</div>
+				</div>
+			</div>
+
+		<!--ROW 4- THIRD CARD APPEARS HERE -->
+		
+		<div class="col-md-4">
+			<div class="card">
+				<img class="card-img-top" src="img/speakersnewdimensions/Dr_Juan_Asensio_New.png">
+				<div class="card-body">
+					<h4 class="card-title"> Dr. Juan Asensio</h4>
+					<p class="card-text"> Professor, Creighton University, USA</p>
+					<a href="asensio.php" class="btn btn-outline-secondary">See Profile</a>
+				</div>
+			</div>
+		</div>
+
+	</div>
+<!-- ROW 4 ENDS -------------------------------------------------->
+
+<!-- ROW 5 BEGINS -------------------------------------------------->
+    <div class="row padding">
+
+        <!-- ROW -5 -FIRST CARD APPEARS HERE -->
+        <div class="col-md-4">
+            <div class="card">
+                <img class="card-img-top" src="img/speakersnewdimensions/Praveen_Vemula_New.png">
+                <div class="card-body">
+                    <h4 class="card-title"> Dr. Praveen Vemula Ph.D.</h4>
+                    <p class="card-text"> Faculty at the Institute for Stem Cell Biology and Regenerative Medicine(inStem), Bangalore, India.</p>
+                    <a href="vemula.php" class="btn btn-outline-secondary">See Profile</a>
+                </div>
+            </div>
+        </div>
+
+    <!-- ROW 5 - SECOND CARD APPEARS HERE -->
+        <div class="col-md-4">
+            <div class="card">
+                <img class="card-img-top" src="img/speakersnewdimensions/Praveen_Vemula_New.png">
+                <div class="card-body">
+                    <h4 class="card-title"> Dr. Praveen Vemula Ph.D.</h4>
+                    <p class="card-text"> Faculty at the Institute for Stem Cell Biology and Regenerative Medicine(inStem), Bangalore, India.</p>
+                    <a href="vemula.php" class="btn btn-outline-secondary">See Profile</a>
+                </div>
+            </div>
+        </div>
+
+    <!--ROW 5- THIRD CARD APPEARS HERE -->
+    <div class="col-md-4">
+            <div class="card">
+                <img class="card-img-top" src="img/speakersnewdimensions/Praveen_Vemula_New.png">
+                <div class="card-body">
+                    <h4 class="card-title"> Dr. Praveen Vemula Ph.D.</h4>
+                    <p class="card-text"> Faculty at the Institute for Stem Cell Biology and Regenerative Medicine(inStem), Bangalore, India.</p>
+                    <a href="vemula.php" class="btn btn-outline-secondary">See Profile</a>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- ROW 5 ENDS -------------------------------------------------->
+
+
+<!-- ROW 6 BEGINS -------------------------------------------------->
+<div class="row padding">
+
+    <!-- ROW -6 -FIRST CARD APPEARS HERE -->
+<div class="col-md-4">
+    <div class="card">
+        <img class="card-img-top" src="img/speakersnewdimensions/Praveen_Vemula_New.png">
+        <div class="card-body">
+            <h4 class="card-title"> Dr. Praveen Vemula Ph.D.</h4>
+            <p class="card-text"> Faculty at the Institute for Stem Cell Biology and Regenerative Medicine(inStem), Bangalore, India.</p>
+            <a href="vemula.php" class="btn btn-outline-secondary">See Profile</a>
+        </div>
+    </div>
+</div>
+
+<!-- ROW 6 - SECOND CARD APPEARS HERE -->
+<div class="col-md-4">
+    <div class="card">
+        <img class="card-img-top" src="img/speakersnewdimensions/Praveen_Vemula_New.png">
+        <div class="card-body">
+            <h4 class="card-title"> Dr. Praveen Vemula Ph.D.</h4>
+            <p class="card-text"> Faculty at the Institute for Stem Cell Biology and Regenerative Medicine(inStem), Bangalore, India.</p>
+            <a href="vemula.php" class="btn btn-outline-secondary">See Profile</a>
+        </div>
+    </div>
+</div>
+
+<!--ROW 6- THIRD CARD APPEARS HERE -->
+<div class="col-md-4">
+    <div class="card">
+        <img class="card-img-top" src="img/speakersnewdimensions/Praveen_Vemula_New.png">
+        <div class="card-body">
+            <h4 class="card-title"> Dr. Praveen Vemula Ph.D.</h4>
+            <p class="card-text"> Faculty at the Institute for Stem Cell Biology and Regenerative Medicine(inStem), Bangalore, India.</p>
+            <a href="vemula.php" class="btn btn-outline-secondary">See Profile</a>
+        </div>
+    </div>
+</div>
+</div>
+<!-- ROW 6 ENDS -------------------------------------------------->
+
+  <!-- ROW 7 BEGINS -------------------------------------------------->
+  <div class="row padding">
+
+<!-- ROW -7 -FIRST CARD APPEARS HERE -->
+<div class="col-md-4">
+    <div class="card">
+        <img class="card-img-top" src="img/speakersnewdimensions/Praveen_Vemula_New.png">
+        <div class="card-body">
+            <h4 class="card-title"> Dr. Praveen Vemula Ph.D.</h4>
+            <p class="card-text"> Faculty at the Institute for Stem Cell Biology and Regenerative Medicine(inStem), Bangalore, India.</p>
+            <a href="vemula.php" class="btn btn-outline-secondary">See Profile</a>
+        </div>
+    </div>
+</div>
+
+<!-- ROW 7 - SECOND CARD APPEARS HERE -->
+<div class="col-md-4">
+    <div class="card">
+        <img class="card-img-top" src="img/speakersnewdimensions/Praveen_Vemula_New.png">
+        <div class="card-body">
+            <h4 class="card-title"> Dr. Praveen Vemula Ph.D.</h4>
+            <p class="card-text"> Faculty at the Institute for Stem Cell Biology and Regenerative Medicine(inStem), Bangalore, India.</p>
+            <a href="vemula.php" class="btn btn-outline-secondary">See Profile</a>
+        </div>
+    </div>
+</div>
+
+<!--ROW 7- THIRD CARD APPEARS HERE -->
+<div class="col-md-4">
+    <div class="card">
+        <img class="card-img-top" src="img/speakersnewdimensions/Praveen_Vemula_New.png">
+        <div class="card-body">
+            <h4 class="card-title"> Dr. Praveen Vemula Ph.D.</h4>
+            <p class="card-text"> Faculty at the Institute for Stem Cell Biology and Regenerative Medicine(inStem), Bangalore, India.</p>
+            <a href="vemula.php" class="btn btn-outline-secondary">See Profile</a>
+        </div>
+    </div>
+</div>
+</div>
+<!-- ROW 7 ENDS -------------------------------------------------->
+
+ <!-- ROW 8 BEGINS -------------------------------------------------->
+ <div class="row padding">
+
+<!-- ROW -8 -FIRST CARD APPEARS HERE -->
+<div class="col-md-4">
+    <div class="card">
+        <img class="card-img-top" src="img/speakersnewdimensions/Praveen_Vemula_New.png">
+        <div class="card-body">
+            <h4 class="card-title"> Dr. Praveen Vemula Ph.D.</h4>
+            <p class="card-text"> Faculty at the Institute for Stem Cell Biology and Regenerative Medicine(inStem), Bangalore, India.</p>
+            <a href="vemula.php" class="btn btn-outline-secondary">See Profile</a>
+        </div>
+    </div>
+</div>
+
+<!-- ROW 8 - SECOND CARD APPEARS HERE -->
+<div class="col-md-4">
+    <div class="card">
+        <img class="card-img-top" src="img/speakersnewdimensions/Praveen_Vemula_New.png">
+        <div class="card-body">
+            <h4 class="card-title"> Dr. Praveen Vemula Ph.D.</h4>
+            <p class="card-text"> Faculty at the Institute for Stem Cell Biology and Regenerative Medicine(inStem), Bangalore, India.</p>
+            <a href="vemula.php" class="btn btn-outline-secondary">See Profile</a>
+        </div>
+    </div>
+</div>
+
+<!--ROW 8- THIRD CARD APPEARS HERE -->
+<div class="col-md-4">
+    <div class="card">
+        <img class="card-img-top" src="img/speakersnewdimensions/Praveen_Vemula_New.png">
+        <div class="card-body">
+            <h4 class="card-title"> Dr. Praveen Vemula Ph.D.</h4>
+            <p class="card-text"> Faculty at the Institute for Stem Cell Biology and Regenerative Medicine(inStem), Bangalore, India.</p>
+            <a href="vemula.php" class="btn btn-outline-secondary">See Profile</a>
+        </div>
+    </div>
+</div>
+</div>
+<!-- ROW 8 ENDS -------------------------------------------------->
+
+  <!-- ROW 9 BEGINS -------------------------------------------------->
+  <div class="row padding">
+
+<!-- ROW -9 -FIRST CARD APPEARS HERE -->
+<div class="col-md-4">
+    <div class="card">
+        <img class="card-img-top" src="img/speakersnewdimensions/Praveen_Vemula_New.png">
+        <div class="card-body">
+            <h4 class="card-title"> Dr. Praveen Vemula Ph.D.</h4>
+            <p class="card-text"> Faculty at the Institute for Stem Cell Biology and Regenerative Medicine(inStem), Bangalore, India.</p>
+            <a href="vemula.php" class="btn btn-outline-secondary">See Profile</a>
+        </div>
+    </div>
+</div>
+
+<!-- ROW 9 - SECOND CARD APPEARS HERE -->
+<div class="col-md-4">
+    <div class="card">
+        <img class="card-img-top" src="img/speakersnewdimensions/Praveen_Vemula_New.png">
+        <div class="card-body">
+            <h4 class="card-title"> Dr. Praveen Vemula Ph.D.</h4>
+            <p class="card-text"> Faculty at the Institute for Stem Cell Biology and Regenerative Medicine(inStem), Bangalore, India.</p>
+            <a href="vemula.php" class="btn btn-outline-secondary">See Profile</a>
+        </div>
+    </div>
+</div>
+
+<!--ROW 9- THIRD CARD APPEARS HERE -->
+<div class="col-md-4">
+    <div class="card">
+        <img class="card-img-top" src="img/speakersnewdimensions/Praveen_Vemula_New.png">
+        <div class="card-body">
+            <h4 class="card-title"> Dr. Praveen Vemula Ph.D.</h4>
+            <p class="card-text"> Faculty at the Institute for Stem Cell Biology and Regenerative Medicine(inStem), Bangalore, India.</p>
+            <a href="vemula.php" class="btn btn-outline-secondary">See Profile</a>
+        </div>
+    </div>
+</div>
+</div>
+<!-- ROW 9 ENDS -------------------------------------------------->
 </div>
 <!-- -------------------------------------------------->
 
