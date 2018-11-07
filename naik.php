64d38affbca521d40e0ef99cd80b8903f6ade3d8
lydiafernandes <fdes.lydia@gmail.com>
2018-11-07 12:25:12 +0530

    speaker version1

--- naik.php
+++ naik.php
@@ -94,8 +94,7 @@ Her basic research is focused on cell motility related signal transduction event
 especially chronic myeloid leukemia, breast cancer, glioblastoma and neural stem cells. 
 Studies of her group have identified rhoGTPases as therapeutic targets. Currently, she is also involved in research on 
 venom and anti-venom, especially from big four snakes.
-</p>
-<p>
+<br>
 Dr. Naik did her Ph.D. at the Cancer Research Institute (CRI) of the <strong>Tata Memorial Centre </strong>
 which is a pioneering and leading comprehensive cancer centre of India.
 She did her post - doctoral research at the Centre 
@@ -114,8 +113,7 @@ ember of the Academic Committee
 , Institutional 
 Scientific Committee, etc
 .
-</p>
-<p>
+<br>
 
 The NCE research
 division 
@@ -171,8 +169,7 @@ precli
 nical
 imaging facility required for the drug discovery 
 industry.
-</p>
-<p>
+<br>
 Dr. Naik 
 is inventor in
 international patents and is a recipient of several national and international awards 
@@ -210,171 +207,7 @@ the
 founder president of the ACTREC Alumni Association
 and is also 
 associated with the Moving Academy of 
-Medicine and Biomedicine
-. Nishigandha
-Naik is working as a 
-Director 
-(additional charge) 
-at the 
-Haffkine Institute for Training, Research 
-and Testing
-(HITRT)
-, Mumbai
-and is the Head of the Dept. of Biochemistry and Dept. of Toxicology.
-She has 
-more than t
-wenty eight
-years of 
-research 
-work experience that includes academia as well as industry. Her work 
-is in the field of cancer cell biology, imaging and pre
--
-clinical drug discovery. 
-Her
-basic research is focused on 
-cell motility related signal transduction e
-vents 
-and
-development of therapeutic approaches for cancer, 
-especially chronic myeloid leukemia, breast cancer
-,
-glioblastoma and neural stem cells
-. 
-S
-tudies
-of her group
-have identified rho
-GTPases as therapeutic targets. 
-Currently, she is also involved in research on 
-venom and 
-anti
--
-venom, especially 
-from 
-big four 
-snakes
-.
-Dr. Naik did her Ph.D. at the Canc
-er Research Institute
-(CRI) 
-of the 
-Tata Memorial 
-C
-entre
-which is a 
-pioneering and leading comprehensive cancer centre of India
-.
-She did her post
--
-doctoral research at the Centre 
-d’Etudes Nucleaires de Grenoble, France. She worked as an independent scientist
-at the CRI (now known as 
-Advanced Centre for Training, Research and Education in Cancer 
-–
-ACTREC). Focus of her group was to 
-understand pathogenesis of chronic myeloid leukemia and breast cancer. In her tenure of almost two decades 
-at the CRI, she develop
-ed expertise in the field of imaging. She was instrumental in establishing Laser 
-Confocal Facility for the institute which was 
-second in the Country. She also contributed to Flow Cytometry and 
-Live Cell Imaging Facilities of the Institute and served as a m
-ember of the Academic Committee
-, Institutional 
-Scientific Committee, etc
-.
-The NCE research
-division 
-of 
-the 
-Piramal Enterprises Limited
-was 
-focus
-ing
-on discovery and development of 
-new drugs to treat unmet medical needs in the areas of cancer, diabetes and
-inflammation. The 
-C
-entre ha
-d
-comprehensive capabilities spanning target identification, lead discovery 
-&
-optimization to pre
--
-clinical 
-development and clinical R&D.
-As a Sr. Group Leader, Dr. Naik
-initiated
-her work
-from establishment of 
-her 
-group and 
-t
-he
-laboratory too. She undertook task
-s
-from target identification
-, discovery and
-development of lead 
-compound 
-to
-the preclinical stage. 
-One of t
-he compound
-s
-has 
-r
-eached
-in 
-clinical trials
-. 
-As a Head of Imaging 
-and Cancer Biology, s
-he ha
-d
-set up complete
-precli
-nical
-imaging facility required for the drug discovery 
-industry.
-Dr. Naik 
-is inventor in
-international patents and is a recipient of several national and international awards 
-including 
-Indian National Science Academy's Medal for Young Scientist,
-award for
-Young Scientist from 
-International Federation for Cell Biology (IFCB) and European Cell Biology Organization (ECBO),
-Marie Curie 
-F
-ellowship 
-from
-the Commission of European Communities and D
-S
-T, Long Term Overseas Associateship 
-award from DBT
-, STA 
-F
-ellowshi
-p from Government of Japan and DST, etc. Dr. Naik has 
-several times 
-served 
-as
-elected
-office
--
-bearer 
-of
-the
-Indian Association for Cancer Research and 
-Indian Society of Cell Biology
-. She
-is 
-the 
-founder president of the ACTREC Alumni Association
-and is also 
-associated with the Moving Academy of 
-Medicine and Biomedicine
-. 
+Medicine and Biomedicine.
      
             <a href="#top" class="btn btn-outline-light btn-lg">Top</a>
     
