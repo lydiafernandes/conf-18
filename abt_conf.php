4be2c3f9bb7eba8115e6ad7d8c59591cefa88134
lydiafernandes <fdes.lydia@gmail.com>
2019-01-05 20:16:29 +0530

    Conference schedule

--- abt_conf.php
+++ abt_conf.php
@@ -25,7 +25,7 @@
     <!-- include the nav bar -->
 <?php include("includes/navigation1.php");?>
 
-<H4 align="center"> click <a href="files/Conference_Schedule_ISC_2019">here</a> to download schedule </H4>
+<H3 align="center"> click <a href="files/Conference_Schedule_ISC_2019">here</a> to download schedule </H3>
  
 <!-- -------------------------------------------------->
 
